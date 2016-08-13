b5df9dedb778a095d4873c7dfda83d255aca92dc
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2016-08-13 21:01:59 +0900

    ARG_CHECK is added

--- C/errorcheck.h
+++ C/errorcheck.h
@@ -9,14 +9,6 @@
 #define errorcheck_h
 #include <stdlib.h>
 
-#define ANSI_COLOR_RED      "\x1b[31m"
-#define ANSI_COLOR_GREEN    "\x1b[32m"
-#define ANSI_COLOR_YELLOW   "\x1b[33m"
-#define ANSI_COLOR_BLUE     "\x1b[34m"
-#define ANSI_COLOR_MAGENTA  "\x1b[35m"
-#define ANSI_COLOR_CYAN     "\x1b[36m"
-#define ANSI_COLOR_RESET    "\x1b[0m"
-
 /* Error Checking Macro *
  *
  * ERROR_MSG(msg);
@@ -35,6 +27,14 @@
  * ARG_CHECK(number);
  *      if argc != number, print error message and exit program.
  */
+
+#define ANSI_COLOR_RED      "\x1b[31m"
+#define ANSI_COLOR_GREEN    "\x1b[32m"
+#define ANSI_COLOR_YELLOW   "\x1b[33m"
+#define ANSI_COLOR_BLUE     "\x1b[34m"
+#define ANSI_COLOR_MAGENTA  "\x1b[35m"
+#define ANSI_COLOR_CYAN     "\x1b[36m"
+#define ANSI_COLOR_RESET    "\x1b[0m"
 
 #define ERROR_MSG(msg) fprintf(stderr, ANSI_COLOR_RED "\tERROR: %s" ANSI_COLOR_RESET, msg)
 #define MEM_ERR "INSUFFICIENT MEMORY\n"
